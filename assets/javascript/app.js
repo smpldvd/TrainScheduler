2da4560665799cdd8c38cda9cdda0730fcbea699
David Luu <David.n.luu@gmail.com>
2018-06-13 10:06:20 -0500

    Initial Commit
    Added files and folders to initial commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyContasDRIIKPyHU5SeY-j509LECZ5M2U8",
+    authDomain: "trainapp-5644b.firebaseapp.com",
+    databaseURL: "https://trainapp-5644b.firebaseio.com",
+    projectId: "trainapp-5644b",
+    storageBucket: "trainapp-5644b.appspot.com",
+    messagingSenderId: "436735871897"
+  };
+  
+  firebase.initializeApp(config);
